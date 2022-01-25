598b0d9414b6d51a5332a82d5a321bdeb2545751
Heina <heina2ffect@gmail.com>
2022-01-25 23:13:45 +0900

    Add Modal function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-/* eslint-disable */
 import React, {useState} from 'react';
 import 중요데이터 from './data.js'
 import logo from './logo.svg';
 import main1 from './img/common1.jpg';
 import main2 from './img/common2.jpg';
 import main3 from './img/common3.jpg';
-import {Navbar, Container, Nav, Carousel} from 'react-bootstrap';
+import {Navbar, Container, Nav, Carousel, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
 
   let [shoes, shoes변경] = useState(중요데이터);
+  let [modal, modal변경] = useState(false);
 
   return (
     <div className="App">
@@ -65,7 +65,7 @@ function App() {
         <div className="row">
         <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            <h4>{shoes[0].title}</h4>
+            <h4 onClick={()=>{modal변경(modal=true)}}>{shoes[0].title}</h4>
             <p>{shoes[0].content}</p>
             <p>{shoes[0].price}</p>
           </div>
@@ -83,8 +83,29 @@ function App() {
           </div>
         </div>
       </div>
+      { modal === true ? <Modal/> : null }
     </div>
   );
 }
 
+function Modal(){
+  return (
+    <div className="modal">
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+  )
+}
 export default App;
