c91085077272d50cf6bf52d8780b8f2fae9c0ecc
Heina <heina2ffect@gmail.com>
2022-02-01 23:49:03 +0900

    styal update

--- src/Detail.js
+++ src/Detail.js
@@ -16,9 +16,8 @@ function Detail(props) {
             </div>
             <div className="col-md-6 mt-4">
               <h4 className="pt-5">{찾은상품.title}</h4>
-              <p>{찾은상품.content}</p>
-              <p>{찾은상품.price}원</p>
               <button className="btn btn-danger">주문하기</button> 
+              <button className="btn btn-danger">장바구니</button> 
               <button className="btn btn-danger" onClick={ ()=>{ 
                   history.push('/');
                } }>뒤로가기</button> 
